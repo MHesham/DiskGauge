0aededfd15c1b5fd0660e4bb1095d397efad171e
MHesham <mhesham.fcis@gmail.com>
2016-05-11 06:24:16 -0700

    Refactoring and clean-up

--- main.cpp
+++ main.cpp
@@ -7,14 +7,9 @@ class DiskGauage
 {
 public:
     DiskGauage() :
-        diskHandle(INVALID_HANDLE_VALUE),
-        avgSectorReadTimeUs(0),
-        avgSectorWriteTimeUs(0),
-        allSectorsReadTimeUs(0),
-        allSectorsWriteTimeUs(0),
-        maxSectorReadTimeUs(0),
-        maxSectorWriteTimeUs(0)
+        diskHandle(INVALID_HANDLE_VALUE)
     {
+        ZeroMemory(&stats, sizeof(stats));
     }
 
     ~DiskGauage()
@@ -28,37 +23,88 @@ public:
 
         DiskGauage dg;
 
-        if (argc < 3) {
+        if (argc < 2) {
             DiskGauage::usage();
             return 0;
         }
 
-        wchar_t* physicalDiskPath(argv[1]);
-        dg.Init(physicalDiskPath);
+        struct _PARAMS {
+            wchar_t* PhysicalDiskPath;
+            enum {
+                ListDisks,
+                Gauge,
+                Burn
+            } Command;
+            LONGLONG SectorIdx;
+        } params;
 
-        if (!wcscmp(argv[2], L"-g")) {
-            dg.gauge();
+
+        if (!wcscmp(argv[1], L"-l")) {
+            params.Command = _PARAMS::ListDisks;
+        } else if (!wcscmp(argv[1], L"-g")) {
+            params.Command = _PARAMS::Gauge;
         } else if (!wcscmp(argv[2], L"-b")) {
-            if (argc < 4) {
-                dg.usage();
+            params.Command = _PARAMS::Burn;
+        } else {
+            dg.usage();
+            return 0;
+        }
+
+        switch (params.Command) {
+        case _PARAMS::ListDisks:
+            dg.enumPhysicalDisks();
+            break;
+
+        case _PARAMS::Gauge:
+        case _PARAMS::Burn:
+            if (argc < 3) {
+                DiskGauage::usage();
                 return 0;
             }
 
-            LONGLONG sectorIdx = std::stoll(wstring(argv[3]));
-            dg.burn(sectorIdx);
-        } else {
-            dg.usage();
+            params.PhysicalDiskPath = argv[2];
+
+            if (params.Command == _PARAMS::Gauge) {
+                dg.gauge(params.PhysicalDiskPath);
+            } else {
+                params.SectorIdx = std::stoll(wstring(argv[3]));
+                dg.burn(params.PhysicalDiskPath, params.SectorIdx);
+            }
+            break;
+
+        default:
+            LogError("Invalid command-line parameters");
+            DiskGauage::usage();
         }
 
         return 0;
     }
 
-    void Init(wchar_t* physicalDiskPath)
-    {
-        LogInfo("Physical Disk Path: %s", physicalDiskPath);
+private:
 
+    void enumPhysicalDisks()
+    {
+        wchar_t physicalDiskNPath[32];
+        for (ULONG n = 0; n < 16; ++n) {
+            swprintf_s(physicalDiskNPath, L"\\\\.\\PhysicalDrive%lu", n);
+
+            if (!openPhysicalDisk(physicalDiskNPath, false)) {
+                continue;
+            }
+
+            if (getDiskGeometry()) {
+                LogInfo("\n%s", physicalDiskNPath);
+                printDiskGeometry();
+            }
+
+            SAFE_CLOSE(diskHandle);
+        }
+    }
+
+    bool openPhysicalDisk(wchar_t* PhysicalDiskPath, bool Verbose)
+    {
         diskHandle = CreateFile(
-            physicalDiskPath,
+            PhysicalDiskPath,
             GENERIC_READ | GENERIC_WRITE,
             FILE_SHARE_READ | FILE_SHARE_WRITE,
             NULL,
@@ -66,194 +112,174 @@ public:
             FILE_FLAG_WRITE_THROUGH | FILE_FLAG_NO_BUFFERING,
             NULL);
         if (diskHandle == INVALID_HANDLE_VALUE) {
-            LogError("CreateFile failed. Error=%ld", GetLastError());
+            if (Verbose) {
+                LogError("CreateFile failed. Error = %ld", GetLastError());
+            }
+            return false;
+        }
+
+        return true;
+    }
+
+    bool burn(wchar_t* PhysicalDiskPath, LONGLONG SectorIdx)
+    {
+        LogInfo("Burn start on %s disk for sector %lld", PhysicalDiskPath, SectorIdx);
+
+        if (!openPhysicalDisk(PhysicalDiskPath, true)) {
+            return false;
         }
 
         if (!getDiskGeometry()) {
-            return;
+            return false;
         }
 
         printDiskGeometry();
 
-        sectorsCount =
-            diskGeometry.Cylinders.QuadPart *
-            (ULONG)diskGeometry.TracksPerCylinder *
-            (ULONG)diskGeometry.SectorsPerTrack;
+        writeBuffer.reset(new BYTE[diskGeometry.BytesPerSector]);
+        readBuffer.reset(new BYTE[diskGeometry.BytesPerSector]);
 
-        LogInfo("Boosting process class and thread priority");
-
-        if (!SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS)) {
-            LogError("SetPriorityClass failed. Error = %ld", GetLastError());
-            return;
+        if (!boostPriority()) {
+            return false;
         }
 
-        if (!SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL)) {
-            LogError("SetThreadPriority failed. Error = %ld", GetLastError());
-            return;
-        }
-    }
-
-private:
-
-    void burn(LONGLONG sectorIdx)
-    {
-        LogInfo("Burn start for sector %lld", sectorIdx);
-
-        const LONGLONG maxCycles = 1000000;
-        const size_t sectorBufferSize = diskGeometry.BytesPerSector;
-        unique_ptr<BYTE[]> writeBuffer(new BYTE[sectorBufferSize]);
-        unique_ptr<BYTE[]> readBuffer(new BYTE[sectorBufferSize]);
-        DWORD numBytesWritten;
-        LARGE_INTEGER distanceToMove;
-        distanceToMove.QuadPart = sectorIdx * diskGeometry.BytesPerSector;
+        const LONGLONG maxCycles = MAXLONGLONG;
 
         for (LONGLONG cycle = 0; cycle < maxCycles; ++cycle) {
-            if (!GenerateRandomDataBuffer(writeBuffer.get(), sectorBufferSize)) {
-                LogError("Failed to generate random buffer");
-                return;
+
+            if (!sectorWriteReadVerify(SectorIdx)) {
+                return false;
             }
 
-            if (!SetFilePointerEx(diskHandle, distanceToMove, NULL, FILE_BEGIN)) {
-                LogError("SetFilePointerEx failed. Error = %ld", GetLastError());
-            }
-
-            if (!WriteFile(
-                diskHandle,
-                writeBuffer.get(),
-                sectorBufferSize,
-                &numBytesWritten,
-                NULL)) {
-                LogError(
-                    "WriteFile failed at sector#%lld. Error = %ld",
-                    sectorIdx,
-                    GetLastError());
-                break;
-            }
-
-            if (!SetFilePointerEx(diskHandle, distanceToMove, NULL, FILE_BEGIN)) {
-                LogError("SetFilePointerEx failed. Error = %ld", GetLastError());
-                break;
-            }
-
-            if (!ReadFile(
-                diskHandle,
-                readBuffer.get(),
-                sectorBufferSize,
-                &numBytesWritten,
-                NULL)) {
-                LogError(
-                    "WriteFile failed at sector#%lld. Error = %ld",
-                    sectorIdx,
-                    GetLastError());
-            }
-
-            if (!isBuffersIdentical(writeBuffer.get(), readBuffer.get(), sectorBufferSize)) {
-                LogError("Binary integrity failed at sector#%lld", sectorIdx);
-                break;
-            }
-
-            // Display statistics every 512 block
-            if (sectorIdx % 1000 == 0) {
+            if (SectorIdx % 1000 == 0) {
                 LogInfo(
                     "Burnt %lld write/read cycle(s)",
                     cycle + 1);
             }
         }
+
+        return true;
     }
 
-    void gauge()
+    bool gauge(wchar_t* PhysicalDiskPath)
     {
-        LogInfo("Gauging disk start for %lld sectors", sectorsCount);
+        LogInfo("Gauging %s disk start for %lld sectors", PhysicalDiskPath, sectorsCount);
 
-        const size_t sectorBufferSize = diskGeometry.BytesPerSector;
-        unique_ptr<BYTE[]> writeBuffer(new BYTE[sectorBufferSize]);
-        unique_ptr<BYTE[]> readBuffer(new BYTE[sectorBufferSize]);
-        DWORD numBytesWritten;
+        if (!openPhysicalDisk(PhysicalDiskPath, true)) {
+            return false;
+        }
 
-        HpcTimer readTimer;
-        HpcTimer writeTimer;
+        if (!getDiskGeometry()) {
+            return false;
+        }
 
-        LARGE_INTEGER distanceToMove;
+        printDiskGeometry();
+
+        writeBuffer.reset(new BYTE[diskGeometry.BytesPerSector]);
+        readBuffer.reset(new BYTE[diskGeometry.BytesPerSector]);
+
+        if (!boostPriority()) {
+            return false;
+        }
 
         for (LONGLONG sectorIdx = 0; sectorIdx < sectorsCount; ++sectorIdx) {
 
-            if (!GenerateRandomDataBuffer(writeBuffer.get(), sectorBufferSize)) {
-                LogError("Failed to generate random buffer");
-                return;
-            }
-
-            distanceToMove.QuadPart = sectorIdx * diskGeometry.BytesPerSector;
-            if (!SetFilePointerEx(diskHandle, distanceToMove, NULL, FILE_BEGIN)) {
-                LogError("SetFilePointerEx failed. Error = %ld", GetLastError());
-            }
-
-            writeTimer.Start();
-            if (!WriteFile(
-                diskHandle,
-                writeBuffer.get(),
-                sectorBufferSize,
-                &numBytesWritten,
-                NULL)) {
-                LogError(
-                    "WriteFile failed at sector#%lld. Error = %ld",
-                    sectorIdx,
-                    GetLastError());
-            }
-            writeTimer.Stop();
-            maxSectorWriteTimeUs = max(maxSectorWriteTimeUs, writeTimer.ElapsedMicroseconds());
-            allSectorsWriteTimeUs += writeTimer.ElapsedMicroseconds();
-            avgSectorWriteTimeUs = allSectorsWriteTimeUs / (sectorIdx + 1);
-
-            distanceToMove.QuadPart = sectorIdx * diskGeometry.BytesPerSector;
-            if (!SetFilePointerEx(diskHandle, distanceToMove, NULL, FILE_BEGIN)) {
-                LogError("SetFilePointerEx failed. Error = %ld", GetLastError());
-            }
-
-            readTimer.Start();
-            if (!ReadFile(
-                diskHandle,
-                readBuffer.get(),
-                sectorBufferSize,
-                &numBytesWritten,
-                NULL)) {
-                LogError(
-                    "WriteFile failed at sector#%lld. Error = %ld",
-                    sectorIdx,
-                    GetLastError());
-            }
-            readTimer.Stop();
-            allSectorsReadTimeUs += readTimer.ElapsedMicroseconds();
-            avgSectorReadTimeUs = allSectorsReadTimeUs / (sectorIdx + 1);
-            maxSectorReadTimeUs = max(maxSectorReadTimeUs, readTimer.ElapsedMicroseconds());
-
-            if (!isBuffersIdentical(writeBuffer.get(), readBuffer.get(), sectorBufferSize)) {
-                LogError("Binary integrity failed at sector#%lld", sectorIdx);
-            }
+            (void)sectorWriteReadVerify(sectorIdx);
 
             // Display statistics every 512 block
             if (sectorIdx % 512 == 0) {
                 LogInfo(
                     "Verified %lldKB. Write: Max=%lldus, Avg=%lldus. Read: Max=%lldus, Avg=%lldus",
                     ((sectorIdx * diskGeometry.BytesPerSector) / 1024),
-                    maxSectorWriteTimeUs,
-                    avgSectorWriteTimeUs,
-                    maxSectorReadTimeUs,
-                    avgSectorReadTimeUs);
+                    stats.MaxSectorWriteTimeUs,
+                    stats.AvgSectorWriteTimeUs,
+                    stats.MaxSectorReadTimeUs,
+                    stats.AvgSectorReadTimeUs);
             }
         }
+        
+        return true;
     }
 
-    bool GenerateRandomDataBuffer(BYTE* BuffPtr, size_t BufferSize)
+    bool sectorWriteReadVerify(LONGLONG SectorIdx)
+    {
+        DWORD numBytesWritten;
+        LARGE_INTEGER distanceToMove;
+        distanceToMove.QuadPart = SectorIdx * diskGeometry.BytesPerSector;
+
+        if (!generateRandomDataBuffer(writeBuffer.get(), diskGeometry.BytesPerSector)) {
+            LogError("Failed to generate random buffer");
+            return false;
+        }
+
+        distanceToMove.QuadPart = SectorIdx * diskGeometry.BytesPerSector;
+        if (!SetFilePointerEx(diskHandle, distanceToMove, NULL, FILE_BEGIN)) {
+            LogError("SetFilePointerEx failed. Error = %ld", GetLastError());
+            return false;
+        }
+
+        writeTimer.Start();
+        if (!WriteFile(
+            diskHandle,
+            writeBuffer.get(),
+            diskGeometry.BytesPerSector,
+            &numBytesWritten,
+            NULL)) {
+            LogError(
+                "WriteFile failed at sector#%lld. Error = %ld",
+                SectorIdx,
+                GetLastError());
+            return false;
+        }
+        writeTimer.Stop();
+
+        stats.MaxSectorWriteTimeUs = max(stats.MaxSectorWriteTimeUs, writeTimer.ElapsedMicroseconds());
+        stats.AllSectorsWriteTimeUs += writeTimer.ElapsedMicroseconds();
+        stats.AvgSectorWriteTimeUs = stats.AllSectorsWriteTimeUs / (SectorIdx + 1);
+
+        distanceToMove.QuadPart = SectorIdx * diskGeometry.BytesPerSector;
+        if (!SetFilePointerEx(diskHandle, distanceToMove, NULL, FILE_BEGIN)) {
+            LogError("SetFilePointerEx failed. Error = %ld", GetLastError());
+            return false;
+        }
+
+        readTimer.Start();
+        if (!ReadFile(
+            diskHandle,
+            readBuffer.get(),
+            diskGeometry.BytesPerSector,
+            &numBytesWritten,
+            NULL)) {
+            LogError(
+                "WriteFile failed at sector#%lld. Error = %ld",
+                SectorIdx,
+                GetLastError());
+            return false;
+        }
+        readTimer.Stop();
+
+        stats.AllSectorsReadTimeUs += readTimer.ElapsedMicroseconds();
+        stats.AvgSectorReadTimeUs = stats.AllSectorsReadTimeUs / (SectorIdx + 1);
+        stats.MaxSectorReadTimeUs = max(stats.MaxSectorReadTimeUs, readTimer.ElapsedMicroseconds());
+
+        if (!isBinaryIdentical(writeBuffer.get(), readBuffer.get(), diskGeometry.BytesPerSector)) {
+            LogError("Binary integrity failed at sector#%lld", SectorIdx);
+            return false;
+        }
+
+        return true;
+    }
+
+    bool generateRandomDataBuffer(BYTE* BuffPtr, size_t BufferSize)
     {
         return BCRYPT_SUCCESS(
             BCryptGenRandom(
                 nullptr,
                 BuffPtr,
-                BufferSize,
+                ULONG(BufferSize),
                 BCRYPT_USE_SYSTEM_PREFERRED_RNG));
     }
 
-    bool isBuffersIdentical(BYTE* Buff1Ptr, BYTE* Buff2Ptr, size_t BufferSize)
+    bool isBinaryIdentical(BYTE* Buff1Ptr, BYTE* Buff2Ptr, size_t BufferSize)
     {
         BYTE* Buff1EndPtr = Buff1Ptr + BufferSize;
         for (; Buff1Ptr != Buff1EndPtr; ++Buff1Ptr, ++Buff2Ptr) {
@@ -264,16 +290,43 @@ private:
         return true;
     }
 
-    BOOL getDiskGeometry()
+    bool getDiskGeometry()
     {
-        DWORD dummy = 0;
-        return DeviceIoControl(diskHandle,
+        DWORD bytesReturned = 0;
+        if (!DeviceIoControl(diskHandle,
             IOCTL_DISK_GET_DRIVE_GEOMETRY,
             NULL, 0,
             &diskGeometry,
             sizeof(diskGeometry),
-            &dummy,
-            (LPOVERLAPPED)NULL);
+            &bytesReturned,
+            (LPOVERLAPPED)NULL)) {
+            LogError("DeviceIoControl(IOCTL_DISK_GET_DRIVE_GEOMETRY) failed. Error = %ld", GetLastError());
+            return false;
+        }
+
+        sectorsCount =
+            diskGeometry.Cylinders.QuadPart *
+            (LONGLONG)diskGeometry.TracksPerCylinder *
+            (LONGLONG)diskGeometry.SectorsPerTrack;
+
+        return true;
+    }
+
+    bool boostPriority()
+    {
+        LogInfo("Boosting process class and thread priority");
+
+        if (!SetPriorityClass(GetCurrentProcess(), REALTIME_PRIORITY_CLASS)) {
+            LogError("SetPriorityClass failed. Error = %ld", GetLastError());
+            return false;
+        }
+
+        if (!SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL)) {
+            LogError("SetThreadPriority failed. Error = %ld", GetLastError());
+            return false;
+        }
+
+        return true;
     }
 
     void printDiskGeometry()
@@ -289,7 +342,8 @@ private:
             (ULONG)diskGeometry.SectorsPerTrack *
             (ULONG)diskGeometry.BytesPerSector;
 
-        LogInfo("Disk size       = %I64d (Bytes)\n"
+        LogInfo(
+            "Disk size       = %I64d (Bytes)\n"
             "                = %.2f (GB)",
             DiskSize, (double)DiskSize / (1024 * 1024 * 1024));
     }
@@ -297,21 +351,27 @@ private:
     static void usage()
     {
         LogInfo(
-            "DiskGauge <PhysicalDriverPath>\n"
-            "Example: DiskGauge \\\\.\\PhysicalDrive3");
+            "DiskGauge [-l|[-g|-b <PhysicalDriverPath>]]\n"
+            "Example: DiskGauge -g \\\\.\\PhysicalDrive3");
     }
 
-    wchar_t* physicalDiskPath;
     HANDLE diskHandle;
     DISK_GEOMETRY diskGeometry;
     LONGLONG sectorsCount;
 
-    LONGLONG avgSectorWriteTimeUs;
-    LONGLONG allSectorsWriteTimeUs;
-    LONGLONG maxSectorWriteTimeUs;
-    LONGLONG avgSectorReadTimeUs;
-    LONGLONG allSectorsReadTimeUs;
-    LONGLONG maxSectorReadTimeUs;
+    struct _STATISTICS {
+        LONGLONG AvgSectorWriteTimeUs;
+        LONGLONG AllSectorsWriteTimeUs;
+        LONGLONG MaxSectorWriteTimeUs;
+        LONGLONG AvgSectorReadTimeUs;
+        LONGLONG AllSectorsReadTimeUs;
+        LONGLONG MaxSectorReadTimeUs;
+    } stats;
+
+    HpcTimer readTimer;
+    HpcTimer writeTimer;
+    unique_ptr<BYTE[]> writeBuffer;
+    unique_ptr<BYTE[]> readBuffer;;
 };
 
 int wmain(int argc, wchar_t **argv)
